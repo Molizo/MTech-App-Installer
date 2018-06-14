a444fe043b26c03c5c84183ef6df270211229f34
Molizo <visoiu.mihnea@live.com>
2018-06-14 10:18:23 +0300

    Initial commit

---
+++ Installer/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            versionNumberLabel.Text = Properties.Settings.Default.version;
+        }
+
+        private void userDbServerButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Start UserDB Server");
+            Process.Start(@".\apps\userDb\server\Database Server.exe");
+        }
+
+        private void userDbClientButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Start UserDB Client");
+            Process.Start(@".\apps\userDb\client\User Database Client.exe");
+        }
+    }
+}
